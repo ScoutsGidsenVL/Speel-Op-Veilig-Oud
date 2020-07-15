10f8003dad5e279eef6d2815d60959c045af1a99
Tinus Van Langendonck <tvl@scoutsengidsenvlaanderen.be>
2020-07-15 12:23:32 +0200

    Android studio -> Android tools

--- build-android.sh
+++ build-android.sh
@@ -2,7 +2,7 @@ cd application
 
 echo "Installeer adoptopenjdk-8-hotspot en gradle via 'sudo apt install adoptopenjdk-8-hotspot gradle'."
 echo "Gebruik 'sudo update-java-alternatives' om adoptopenjdk-8-hotspot als standaard in te stellen."
-echo "Download android studio en pak uit onder /mnt/c/dev/ ."
+echo "Download android command line tools en pak uit onder /mnt/c/dev/android-tools ."
 
 export JAVA_HOME=/usr/lib/jvm/adoptopenjdk-8-hotspot-amd64
 export ANDROID_HOME=/mnt/c/dev/android-studio
