68a6e8f752ebeacd1eecfb5743cf9fa61bd7b631
Tinus Van Langendonck <tvl@scoutsengidsenvlaanderen.be>
2020-07-15 16:00:15 +0200

    Script vervolledigd

--- build-android.sh
+++ build-android.sh
@@ -1,4 +1,8 @@
-cd application
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname "$0")"/application
 
 echo "Installeer adoptopenjdk-8-hotspot en gradle via 'sudo apt install adoptopenjdk-8-hotspot gradle'."
 echo "Gebruik 'sudo update-java-alternatives' om adoptopenjdk-8-hotspot als standaard in te stellen."
@@ -11,5 +15,6 @@ export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform_tools
 
 node_modules/.bin/ionic cordova build --verbose --release android
 
-echo "Kopieer de apk naar een android-gsm."
-echo "Sign de apk met de app apk-signer."
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /mnt/s/03.\ Ondersteuning/Informatica/Staf-projecten/Speel\ op\ veilig/Google\ Play/speel-op-veilig-release-key.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk speelopveiligkey
+
+$ANDROID_HOME/build-tools/*/zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk SpeelOpVeilig.1.1.0.apk
